1d477ce1018e487c8df2a83adbdb2b1aab53bbd4
Guillaume AMAT <guillaume@amat.io>
2017-11-18 17:12:04 +0100

    chore: Add src and Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const plugins = [
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    mangle: true,
+    output: {
+      comments: false
+    },
+    compress: {
+      warnings: false
+    }
+  })
+];
+
+module.exports = {
+  plugins,
+  devtool: 'source-map',
+  entry: {
+    OsmoseRequest: path.join(__dirname, 'src/index.js')
+  },
+  output: {
+    libraryTarget: 'umd',
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
